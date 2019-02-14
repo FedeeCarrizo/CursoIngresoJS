f64c59f89a2cf09108a9c1cd02a67bab528f788c
FedeeCarrizo <federicoagustincarrizo@gmail.com>
2019-02-14 19:33:05 -0300

    1

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,5 +1,24 @@
 
 function mostrar()
 {
+	var perro_uno
+	var perro_dos
+	var peso_uno
+	var peso_dos
+	var suma
+
+ 	peso_dos=parseInt(peso_dos)
+ 	peso_uno=parseInt(peso_uno)
+
+	perro_uno= Manchita;
+	perro_dos= Tomi;
+	peso_uno=14;
+	peso_dos=18;
+
+	result=peso_uno + peso_dos;
+	suma= result;
+
+	alert("Tenes dos mascotas " perro_uno " y " perro_dos ", que pesan" peso_uno " y" peso_dos "kilos, la suma de los kilos es" suma);
+
 
 }
