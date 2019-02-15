bd4f236e000f8002ab76ce716d6bf66e4ecf0903
FedeeCarrizo <federicoagustincarrizo@gmail.com>
2019-02-15 15:26:18 -0300

    If and Else

--- 2-InstruccionIf/jsInstruccionIF (8).js
+++ 2-InstruccionIf/jsInstruccionIF (8).js
@@ -1,7 +1,22 @@
 function mostrar()
 {
 //tomo la edad  
+	var edad;
+	var estadocivil;
+	var mensaje;
 
+	edad=document.getElementById('edad').value;
+	estadocivil=document.getElementById('estadoCivil').value;
+	edad=parseInt(edad);
+
+	if(edad >18)
+	{
+		if(estadocivil == "Soltero")
+
+			mensaje="Es soltero y no es menor";
+	}
+
+	alert(mensaje);
 	
 
 
