9c8dd5323eefe358d013180224891d48dce67678
FedeeCarrizo <federicoagustincarrizo@gmail.com>
2019-02-15 15:59:02 -0300

    If and Else completo

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,29 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
+	var random;
 	
+	random=Math.floor(Math.random() *10) + 1;
+
+	if(random >8) 
+	{
+		mensaje="Excelente"
+	}
+	else
+	{
+		if(random<4)
+		{
+			mensaje="Vamos, para la proxima se puede"
+		}
+		else
+		{
+			mensaje="Aprobó"
+		}
+	}
+	alert(random+ " "+ mensaje);
+
+
+	
+
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -1,6 +1,11 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+	//Math.random();
+	var random;
+
+	random=Math.floor(Math.random() *10) + 1;
+
+	alert(random);
 
 }//FIN DE LA FUNCIÓN
