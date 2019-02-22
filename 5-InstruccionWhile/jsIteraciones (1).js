0806c5faa075f0c637b70c6df8047a1f3e24894b
FedeeCarrizo <federicoagustincarrizo@gmail.com>
2019-02-21 22:12:04 -0300

    21 de Febrero

--- 5-InstruccionWhile/jsIteraciones (1).js
+++ 5-InstruccionWhile/jsIteraciones (1).js
@@ -1,6 +1,13 @@
 function mostrar()
 {
-	alert('iteración while');
+	var contador;
 
+	contador=0;
 
-}//FIN DE LA FUNCIÓN
+	while(contador<10)
+	{
+		contador=contador + 1;
+		console.log(contador);
+	}
+}
+
