65fa6213f9b90de41e58d9f45bb46ddf1cb6eb4e
FedeeCarrizo <federicoagustincarrizo@gmail.com>
2019-02-21 21:20:17 -0300

    Recu

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
@@ -1,4 +1,14 @@
 function mostrar()
 {
+	var precio;
+	var paquete;
+	var descuento;
+	var pago;
+
+	precio=prompt("Ingrese precio");
+	pago=prompt("Ingrese forma de pago");
+	paquete=prompt("Ingrese paquete");
+
+	
 
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
@@ -1,4 +1,44 @@
 function mostrar()
 {
+	var numero_uno;
+	var numero_dos;
+	var numero_tres;
+	var numero_cuatro;
+	var resultado;
+	var mensaje;
+	var mayor;
 
+
+
+	numero_uno=prompt("Ingrese numero");
+	numero_dos=prompt("Ingrese numero");
+	numero_tres=prompt("Ingrese numero");
+	numero_cuatro=prompt("Ingrese numero");
+
+	numero_uno=parseInt(numero_uno);
+	numero_dos=parseInt(numero_dos);
+	numero_tres=parseInt(numero_tres);
+	numero_cuatro=parseInt(numero_cuatro);
+	resultado=parseInt(resultado);
+
+	resultado = numero_uno + numero_dos + numero_tres + numero_cuatro 
+
+	if (resultado >99)
+	{
+		mensaje=resultado*0.90
+	}
+	else
+	{
+		if(resultado>50) 
+		{
+			mensaje=resultado*0.95
+		}
+		else
+		{
+			mensaje=resultado*1.15
+		}
+	}
+	alert("Tu compra es de $"+mensaje);
 }
+
+
