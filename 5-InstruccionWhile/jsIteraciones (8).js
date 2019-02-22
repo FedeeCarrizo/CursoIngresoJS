1b59176789466e87f5c003ea633a4bd3f25c3911
alumno <alumno>
2019-02-22 19:43:19 -0300

    While

--- 5-InstruccionWhile/jsIteraciones (8).js
+++ 5-InstruccionWhile/jsIteraciones (8).js
@@ -4,11 +4,35 @@ function mostrar()
 	var contador=0;
 	var positivo=0;
 	var negativo=1;
-	
+
 	var respuesta='si';
+	while(1)
 
+	{
+		contador=contador+1;
+		numero=prompt("Ingrese el numero "+contador+" #: ");
+		numero=parseInt(numero);
+		//acumulador=acumulador+ numero;
 
-document.getElementById('suma').value=positivo;
-document.getElementById('producto').value=negativo;
+		respuesta=prompt(" => Si para continuar");
+		if(numero>0)
+		{
+			positivo=positivo*contador
+		}
+		else
+		{
+				negativo=negativo*contador
+		}
+
+		if(respuesta!="si")
+		{
+			break;
+		}
+
+		
+	}
+
+	document.getElementById('suma').value=positivo;
+	document.getElementById('producto').value=negativo;
 
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -2,15 +2,34 @@ function mostrar()
 {
 
 	var contador=0;
-	// declarar variables
-	
+	var maximo;
+	var minimo;
 	var respuesta='si';
 
-	while(respuesta!='no')
+	maximo= -999;
+	minimo= 999;
+
+	while(respuesta=="si")
+
 	{
+		contador=contador+1;
+		numero=prompt("Ingrese el numero "+contador+" #: ");
+		numero=parseInt(numero);
+		if(numero>maximo)
+		{
+			maximo=numero	
+		}
+
+		if(numero<minimo)
+		{
+			minimo=numero
+		}
+
+		respuesta=prompt(" => Si para continuar")
 		
-	
 	}
+	document.getElementById('maximo').value=maximo;
+	document.getElementById('minimo').value=minimo;
 
 
 
