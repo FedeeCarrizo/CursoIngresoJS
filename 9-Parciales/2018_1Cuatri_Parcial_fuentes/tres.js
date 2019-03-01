47451ab474d91fbb2dd29fa340d41b5a0a5184f9
FedeeCarrizo <federicoagustincarrizo@gmail.com>
2019-03-01 01:54:55 -0300

    Practica del 01/03

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,15 @@
 function mostrar()
 {
+	var precio;
+	var descuento;
+	var precioFinal;
+
+	precio=prompt("Ingrese un monto de dinero");
+	descuento=prompt("Ingrese el porcentaje de descuento");
+
+	precioFinal=(descuento*precio)/100
+
+	document.getElementById('elPrecioFinal').value=precioFinal;
+
 
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,17 @@
 
 function mostrar()
 {
+	var ancho;
+	var largo;
+	var perimetro;
 
+	ancho=prompt("Ingrese ancho del rectangulo");
+	largo=prompt("Ingrese largo del rectangulo");
+
+	ancho=parseInt(ancho);
+	largo=parseInt(largo);
+
+	perimetro=ancho*2+largo*2
+
+	alert("El perimetro es de "+perimetro);
 }
